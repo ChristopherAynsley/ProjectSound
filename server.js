fb81aa7b21a11a0f284930b3ca93abc256a3c9ab
up877101 <up877101@myport.ac.uk>
2019-03-05 00:35:15 +0000

    package.json and server.js update
    package.json:
    - added mysql to dependencies
    - still need to manually install mysql i think
    server.js
    - moved port initialisation to config
    - basic database connection using config details

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 8080
+
+/*
+ * project sound Server
+ *
+ * authors: up numbers here
+ */
 
 
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const mysql = require('mysql');
+const express = require('express');
+
+const config = require('./config.js');
+
+const app = express();
+const con = mysql.createConnection(config.mysql);
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database connected succesfully")
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use(express.static("public"));
-app.listen(port, () => console.log(`Server started, listening on port ${port}!`))
+app.listen(config.port, () => console.log(`Server started, listening on port ` + config.port));
