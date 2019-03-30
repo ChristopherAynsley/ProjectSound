119ed0aafbd87574192207bc1aca5d431f84e762
Connor <37509349+C-Porter@users.noreply.github.com>
2019-03-30 13:16:59 +0000

    Created init, improved some functions

--- public/inputHandler.js
+++ public/inputHandler.js
@@ -1,24 +1,44 @@
+//Global Variables
+let errorBox;
+
+//On page load
+function init() {
+  errorBox = document.getElementById('error');
+}
+
 //Tests if group name is valid
-function groupNameValid(name){
-  return (typeof name === "string" && name.length <= 50)
+function groupNameValid(name) {
+  return (typeof name === "string" && name.length <= 50 && name.length > 2)
+}
 
 //Sends new group info to get added to the server
 //TODO Swap else into if and then remove else
-function addGroup(newGroup, owner = "default", visibility = false){
+function addGroup(newGroup, owner = "default", visibility = false) {
+  console.log(newGroup)
   if (groupNameValid(newGroup)) {
     //send to server
-  }
-  else {
-    const errorBox = document.getElementById('error');
-    errorBox.textContent = "Please ensure you enter text content under the length of 50 characters";
+    errorBox.textContent = "";
+    return true;
+  } else {
+    errorBox.textContent =
+      "Please ensure you enter text content under the length of 50 characters";
+    return false;
   }
 }
 
 //Sends a group name to the server to search for groups
-function searchPublicGroups(groupName){
+function searchPublicGroups(groupName) {
   if (!groupNameValid(groupName)) {
-    //TODO finish this
+    errorBox.textContent =
+      "Please ensure you enter text content under the length of 50 characters";
+    return;
   }
+  //Send Search Query to server
 }
 
-module.exports.groupNameValid = groupNameValid
+function handleFiles(file) {
+  //TODO Send file to server and location of file to be placed ect ect
+}
+
+window.onload = init;
+//module.exports.groupNameValid = groupNameValid;
